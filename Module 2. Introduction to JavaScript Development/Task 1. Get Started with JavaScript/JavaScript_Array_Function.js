4fff049b876956032b05e17b40c321a62af4169c
Daniel Molina <dmolcap@gmail.com>
2019-03-27 15:41:01 +0100

    module 2, task 1 extra exercises

--- Module 2. Introduction to JavaScript Development/Task 1. Get Started with JavaScript/JavaScript_Array_Function.js
+++ Module 2. Introduction to JavaScript Development/Task 1. Get Started with JavaScript/JavaScript_Array_Function.js
@@ -1,82 +1,82 @@
 "use strict"
 
 // Exercise 1: Create an array with all the names of your class (including mentors).  Sort the array alphabetically.  Print the first element of the array in the console.  Print the last element of the array in the console.  Print all the elements of the array in the console.  Use a "for" loop.
-// let classNames = ["Raul", "Oscar", "Sergi", "Richard", "Xavi", "Samu", "Patricia", "Daniel"];
+let classNames = ["Raul", "Oscar", "Sergi", "Richard", "Xavi", "Samu", "Patricia", "Daniel"];
 
-// console.log(classNames.sort()); // Print the array sort alphabetically
+console.log(classNames.sort()); // Print the array sort alphabetically
 
-// console.log(classNames[0]); // Print the first element
+console.log(classNames[0]); // Print the first element
 
-// console.log(classNames.slice(-1)[0]); // Print the last element
+console.log(classNames.slice(-1)[0]); // Print the last element
 
-// console.log(classNames[classNames.length - 1]); // Print the last element
+console.log(classNames[classNames.length - 1]); // Print the last element
 
-// for (let i = 0; i < classNames.length; i++) {
-//   console.log(classNames[i]); // Print all the elements
-// }
+for (let i = 0; i < classNames.length; i++) {
+  console.log(classNames[i]); // Print all the elements
+}
 
 // Exercise 2: Create an array with all the ages of the students in your class.  Iterate the array using a while loop, and then print every age in the console.  Add a conditional inside the while loop to only print even numbers.  Change the loop to use a "for" loop instead of a "while" loop.
-// let classAges = [23, 42, 26, 19, 34, 29, 25, 28];
-// let index = 0;
+let classAges = [23, 42, 26, 19, 34, 29, 25, 28];
+let index = 0;
 
-// while (index < classAges.length) {
-//   if (classAges[index] % 2 === 0) {
-//     console.log(classAges[index]);
-//   }
-//   index++;
-// }
+while (index < classAges.length) {
+  if (classAges[index] % 2 === 0) {
+    console.log(classAges[index]);
+  }
+  index++;
+}
 
-// for (let i = 0; i < classAges.length; i++) {
-//   if (classAges[i] % 2 === 0) {
-//     console.log(classAges[i]);
-//   }
-// }
+for (let i = 0; i < classAges.length; i++) {
+  if (classAges[i] % 2 === 0) {
+    console.log(classAges[i]);
+  }
+}
 
 // Exercise 3: Write a function which receives an array as a parameter and prints the lowest number in the array to the console.
 // Exercise 4: Write a function which receives an array as a parameter and prints the biggest number in the array to the console.
-// function lowestNumber(array) {
-//   let min = Math.min(...array);
-//   let max = Math.max(...array);
-//   // let result = "El mínimo es: " + min + ". Y el máximo es: " + max;
-//   return [min, max];
-// }
+function lowestNumber(array) {
+  let min = Math.min(...array);
+  let max = Math.max(...array);
+  // let result = "El mínimo es: " + min + ". Y el máximo es: " + max;
+  return [min, max];
+}
 
-// console.log(lowestNumber(classAges));
+console.log(lowestNumber(classAges));
 
 // Exercise 5: Write a function which receives two parameters, an array and an index.  The function will print the value of the element at the given position (one-based) to the console.
-// var array = [3, 6, 67, 6, 23, 11, 100, 8, 93, 0, 17, 24, 7, 1, 33, 45, 28, 33, 23, 12, 99, 100];
-// var index = 1;
+var array = [3, 6, 67, 6, 23, 11, 100, 8, 93, 0, 17, 24, 7, 1, 33, 45, 28, 33, 23, 12, 99, 100];
+var index = 1;
 
-// function printIndex(array, index) {
-//   let print = array[index];
-//   return print;
-// }
+function printIndex(array, index) {
+  let print = array[index];
+  return print;
+}
 
-// console.log(printIndex(array, index));
+console.log(printIndex(array, index));
 
 // Exercise 6: Write a function which receives an array and only prints the values that repeat.
 let numbs = [3, 6, 67, 6, 23, 11, 100, 8, 93, 0, 17, 24, 7, 1, 33, 45, 28, 33, 23, 12, 99, 100];
 
-// console.log(numbs.includes(numbs[4]));
+console.log(numbs.includes(numbs[4]));
 
 //1. que coja el primer valor del array
 //2. que busque otro valor igual en el array
 //3. si lo hay, que lo imprima
 //4. que vuelva a empezar con el segundo valor
 
-// function printRepeats(array) {
-//   let result = [];
-//   for (let i = 0; i < array.length; i++) {
-//     for (let j = i + 1; j < array.length; j++) {
-//       if (array[i] === array[j] && result.includes(array[i]) === false) {
-//         result.push(array[i]);
-//       }
-//     }
-//   }
-//   return result;
-// }
+function printRepeats(array) {
+  let result = [];
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[i] === array[j] && result.includes(array[i]) === false) {
+        result.push(array[i]);
+      }
+    }
+  }
+  return result;
+}
 
-// console.log(printRepeats(numbs));
+console.log(printRepeats(numbs));
 
 // function printRepeats2(arra1) {
 //   var repeatedCounter = {};
--- Module 2. Introduction to JavaScript Development/Task 1. Get Started with JavaScript/JavaScript_String_Functions.js
+++ Module 2. Introduction to JavaScript Development/Task 1. Get Started with JavaScript/JavaScript_String_Functions.js
@@ -1,38 +1,38 @@
 // Exercise 1: Write a JavaScript function that reverses a number. For example, if x = 32443 then the output should be 34423.
-// let input = prompt("Enter a number to reverse it!!");
+let input = prompt("Enter a number to reverse it!!");
 
-// function reverseMe(itemToReverse) {
-//   let reversed = String(itemToReverse).split("").reverse().join("");
-//   return Math.sign(itemToReverse) * parseFloat(reversed);
-// }
+function reverseMe(itemToReverse) {
+  let reversed = String(itemToReverse).split("").reverse().join("");
+  return Math.sign(itemToReverse) * parseFloat(reversed);
+}
 
-// console.log(reverseMe(input));
+console.log(reverseMe(input));
 
 // Exercise 2: Write a JavaScript function that returns a string in alphabetical order. For example, if x = 'webmaster' then the output should be 'abeemrstw'.  Punctuation and numbers aren't passed in the string.
-// let input = prompt("Enter a string to reverse it!");
+let input = prompt("Enter a string to reverse it!");
 
-// function reverseMe(itemToReverse) {
-//   let reversed = "";
-//   for (let i = itemToReverse.length - 1; i >= 0; i--) {
-//     reversed += itemToReverse[i];
-//   }
-//   return reversed.replace(/[.]/g, "").replace(/[0-9]/g, "");
-// }
+function reverseMe(itemToReverse) {
+  let reversed = "";
+  for (let i = itemToReverse.length - 1; i >= 0; i--) {
+    reversed += itemToReverse[i];
+  }
+  return reversed.replace(/[.]/g, "").replace(/[0-9]/g, "");
+}
 
-// console.log(reverseMe(input));
+console.log(reverseMe(input));
 
 // Exercise 3: Write a JavaScript function that converts the first letter of every word to uppercase. For example, if x = "prince of persia" then the output should be "Prince Of Persia".
-// let input = prompt("Enter a phrase to capitalize the first letter of each word");
+let input = prompt("Enter a phrase to capitalize the first letter of each word");
 
-// function capitalizeMe(stringToCapitalize) {
-//   let lowerCase = String(stringToCapitalize).toLowerCase();
+function capitalizeMe(stringToCapitalize) {
+  let lowerCase = String(stringToCapitalize).toLowerCase();
 
-//   return lowerCase.replace(/(^| )(\w)/g, function capitalize(firstWordLetter) {
-//     return firstWordLetter.toUpperCase();
-//   })
-// }
+  return lowerCase.replace(/(^| )(\w)/g, function capitalize(firstWordLetter) {
+    return firstWordLetter.toUpperCase();
+  })
+}
 
-// console.log(capitalizeMe(input));
+console.log(capitalizeMe(input));
 
 // Exercise 4: Write a JavaScript function that finds the longest word in a phrase. For example, if x = "Web Development Tutorial", then the output should be "Development".
 let input = prompt("Enter a phrase to discover the longest word in it");
